9bd606c3d502e5d96b401a6079c97074b8eb07f8
nero van gestel <nerovg@hotmail.com>
2022-03-10 18:26:18 +0100

    1

---
+++ nero/img/MAP/pokemon silver.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="pokemon silver" tilewidth="32" tileheight="32" tilecount="754" columns="13">
+ <image source="../../../../../Downloads/Game Boy GBC - Pokemon Gold Silver - Tileset Day.png" width="436" height="1870"/>
+</tileset>
---
+++ nero/img/MAP/silver.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="silver" tilewidth="8" tileheight="8" margin="4" tilecount="12582" columns="54">
+ <image source="../../../../../Downloads/Game Boy GBC - Pokemon Gold Silver - Tileset Day.png" width="436" height="1870"/>
+</tileset>
